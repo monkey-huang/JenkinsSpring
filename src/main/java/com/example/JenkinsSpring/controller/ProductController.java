ad283e8acb21a89b982ca379ed633cc78930cd0c
monkey-huang <zxcv45628789@gmail.com>
2021-05-07 10:39:44 +0800

    test

--- src/main/java/com/example/JenkinsSpring/controller/ProductController.java
+++ src/main/java/com/example/JenkinsSpring/controller/ProductController.java
@@ -18,7 +18,7 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
-    @ApiOperation(value = "查詢產品名稱test89")
+    @ApiOperation(value = "查詢產品名稱testF")
 	@RequestMapping(value= "/product", method = RequestMethod.POST)
 	public ResponseProduct findProduct(@RequestBody RequestProduct requestProduct) {
 		System.out.println("test");
